998fac17dc0dae56cd9c6912c52457d02d47039c
nkzren <renan.nakazawa0@gmail.com>
2023-03-08 20:11:33 -0300

    add codigos cap 5 do Learning Go

---
+++ learning-go/ch5/closures.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"fmt"
+	"sort"
+)
+
+type Person struct {
+	FirstName string
+	LastName  string
+	Age       int
+}
+
+func main() {
+	people := []Person{
+		{"Leonardo", "Santos", 25},
+		{"Jorge", "Amado", 90},
+		{"Kleber", "Pereira", 37},
+	}
+	fmt.Println(people)
+
+	sort.Slice(people, func(i int, j int) bool {
+		return people[i].Age < people[j].Age
+	})
+	fmt.Println("Sorted:")
+	fmt.Println(people)
+
+	twoBase := makeMult(2)
+	threeBase := makeMult(3)
+	for i := 0; i < 3; i++ {
+		fmt.Println(twoBase(i), threeBase(i))
+	}
+}
+
+func makeMult(base int) func(int) int {
+	return func(factor int) int {
+		return base * factor
+	}
+}
